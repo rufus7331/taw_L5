9f2b570db8123d79fffbbbeaff0ea62ec8f38383
Hubert <hubertos120@gmail.com>
2023-04-19 22:41:33 +0200

    Laboratorium 6 - NodeJS. Warstwa REST. Tworzenie endpointów. Praca z Postman

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import config from './config';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import routes from './REST/routes';
 
 // Instancja serwera
 const app = express();
@@ -15,6 +16,8 @@ app.use(bodyParser.json({limit: '2048kb'}));
 
 // cors - mechanizm umożliwiający współdzielenie zasobów pomiędzy serwerami znajdującymi się w różnych domenach
 app.use(cors());
+
+routes(app);
 
 
 // uruchomienie serwera
