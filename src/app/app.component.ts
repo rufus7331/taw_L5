704fb916daa4709dd8872edb75044fa966a7343d
Hubert <hubertos120@gmail.com>
2023-05-03 18:56:00 +0200

    lab8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+
+  public counter: number = 0;
+
+  add() {
+    this.counter++;
+  }
+
+  remove() {
+    this.counter--;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppComponentComponent } from './app-component/app-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppComponentComponent
   ],
   imports: [
     BrowserModule,
